061bc292c692fb941200f3665186bd097ebe38de
kalocesa <kalocesa95@gmail.com>
2025-02-10 00:49:58 -0600

    Actualización de readme y de app.js

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Tripleten web_project_around_express
+
+Este proyecto es una API para gestionar usuarios y tarjetas, creada con Express.js. La API permite obtener una lista de todos los usuarios, todas las tarjetas, y buscar usuarios específicos por su ID.
+
+##
+
+Tecnologías Utilizadas
+
+> Node.js: Un entorno de ejecución de JavaScript que permite utilizar JavaScript en el servidor. Es utilizado para construir aplicaciones escalables y de alto rendimiento.
+
+> Express.js: Un framework web para Node.jsque facilita la creación de aplicaciones web y APIs. Proporciona una serie de características robustas para el desarrollo web y móvil.
+
+> Nodemon: Una herramienta que ayuda a desarrollar aplicaciones basadas en Node.jsreiniciando automáticamente el servidor cuando se detectan cambios en los archivos del proyecto.
+
+> ESLint: Una herramienta de análisis de código estático que identifica y reporta patrones en el código, con el objetivo de hacer el código más consistente y evitar errores. En este proyecto, se utiliza con la guía de estilo de Airbnb.
+
+##
--- app.js
+++ app.js
@@ -1,6 +1,41 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const fs = require("fs");
+const path = require("path");
+
+// Leer los archivos JSON
+const users = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "data", "users.json"), "utf-8")
+);
+const cards = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "data", "cards.json"), "utf-8")
+);
+
+// Ruta para obtener todos los usuarios
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// Ruta para obtener todas las tarjetas
+app.get("/cards", (req, res) => {
+  res.json(cards);
+});
+
+// Ruta para obtener un usuario por ID
+app.get("/users/:id", (req, res) => {
+  const user = users.find((u) => u._id === req.params.id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ message: "ID de usuario no encontrado" });
+  }
+});
+
+// Manejar rutas no existentes
+app.use((req, res) => {
+  res.status(404).json({ message: "Recurso solicitado no encontrado" });
+});
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
