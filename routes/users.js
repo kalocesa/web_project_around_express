0f4dffe99f2298abbf47fa4a58add4eaf4e7e187
kalocesa <kalocesa95@gmail.com>
2025-04-17 21:29:16 -0600

    Se genera fix de users.controllers

--- routes/users.js
+++ routes/users.js
@@ -6,35 +6,6 @@ const mongoose = require("mongoose");
 const assert = require("assert");
 const { route } = require("./cards");
 
-//Crear una variable, para guardar el objeto mongoose con el método Schema()
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: [true, `El nombre del usuario es requerido`],
-  },
-  about: {
-    type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: [true, `El acerca del usuario es requerido`],
-  },
-  avatar: {
-    type: String,
-    validate: {
-      validator: function (v) {
-        return /^https?:\/\/.+\.(jpg|jpeg|png|gif|webp)(\?.*)?$/i.test(v);
-      },
-      message: (props) => `${props.value} no es una URL valida`,
-    },
-    required: [true, `El avatar del usuario es requerido`],
-  },
-});
-
-//Guardamos el model/schema de user en la variable objeto User
-const User = mongoose.model("user", userSchema);
-
 router.get("/", async (req, res) => {
   try {
     const users = await User.find();
